4673e282ddfe9fbe37e75c9236063e01a47c9ee9
AnanyaBansal28 <ananya2006bansal@gmail.com>
2026-05-21 14:53:15 +0530

    final deploy setup

--- Mess-management-system-main/backend/models/User.js
+++ Mess-management-system-main/backend/models/User.js
@@ -1,11 +1,30 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  studentID: String,
-  email: String,
-  password: String,
-  role: String
+
+name:{
+type:String
+},
+
+email:{
+type:String
+},
+
+studentID:{
+type:String
+},
+
+password:{
+type:String,
+required:true
+},
+
+role:{
+type:String,
+required:true
+}
+
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports =
+mongoose.model("User", userSchema);
